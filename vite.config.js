eb3bf71cea47d4cb81e248dad0c5c8883f562ced
Jana Pavlasek <jbpavlasek@gmail.com>
2024-08-09 12:18:18 -0400

    Make paths relative on build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '',  // Forces relative paths on build.
   plugins: [react()],
   server: {
     host: '0.0.0.0',
